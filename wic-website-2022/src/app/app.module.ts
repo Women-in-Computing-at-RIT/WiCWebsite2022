eebf6c6348bc49e1bcd2c50793d3bc59ecb8980b
Tina-DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2022-07-06 14:21:38 -0400

    Linter Added

--- wic-website-2022/src/app/app.module.ts
+++ wic-website-2022/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
