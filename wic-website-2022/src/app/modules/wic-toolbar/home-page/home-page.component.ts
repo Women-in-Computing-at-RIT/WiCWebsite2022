270cc0a4fbaf2f5f08e7de92e21f82b671d04eb2
Tina-DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2022-07-26 20:12:51 -0400

    carousel

--- wic-website-2022/src/app/modules/wic-toolbar/home-page/home-page.component.ts
+++ wic-website-2022/src/app/modules/wic-toolbar/home-page/home-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  slides = ['http://women.rit.edu/img/afterhours/afterhours3.jpeg','http://women.rit.edu/img/afterhours/afterhours5.jpeg']
   constructor() { }
 }
