d367a5e4ad2e35a9e0b60079894d6895ea102ce3
Tina-DiLorenzo <56976587+Tina-DiLorenzo@users.noreply.github.com>
2022-09-12 07:31:17 -0400

    Navbar

--- wic-website-2022/src/app/modules/wic-toolbar/wic-toolbar.component.ts
+++ wic-website-2022/src/app/modules/wic-toolbar/wic-toolbar.component.ts
@@ -1,19 +1,24 @@
-import { Component, MissingTranslationStrategy, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import Button from './models/toolbar-button.model';
 import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-wic-toolbar',
   templateUrl: './wic-toolbar.component.html',
-  styleUrls: ['./wic-toolbar.component.scss']
+  styleUrls: ['./wic-toolbar.component.scss'],
 })
-export class WicToolbarComponent {
+export class WicToolbarComponent implements OnInit {
+  isMobile = false;
+  maxWidth = 790;
+  @ViewChild('drawer') sidebar?: MatSidenav;
 
   toolbarButtons: Button[] = [
     {
       text: 'home',
-      links: [{link: '/', text: '', }]
-    }, {
+      links: [{ link: '/', text: '' }],
+    },
+    {
       text: 'about',
       links: [
         { link: 'mission', text: 'mission' },
@@ -21,42 +26,60 @@ export class WicToolbarComponent {
         { link: 'alumni', text: 'alumni' },
         { link: 'scholarships', text: 'scholarships' },
         { link: 'tutoring', text: 'tutoring' }, // TODO: move from this menu later?
-      ]
-    }, {
+      ],
+    },
+    {
       text: 'events',
       links: [
         { link: 'afterhours', text: 'after hours' },
         { link: 'calendar', text: 'upcoming calendar' },
         { link: 'wichacks', text: 'wichacks' },
         { link: 'annualevents', text: 'annual events' },
-      ]
-    }, {
+      ],
+    },
+    {
       text: 'committees',
       links: [
         { link: 'committeeheads', text: 'committee heads' },
         { link: 'projects', text: 'projects' },
         { link: 'allies', text: 'allies' },
         { link: 'outreach', text: 'outreach' },
-      ]
-    },{
-      text: 'blog',
-      links: [{link: 'blog', text: '', }]
-    }, {
-      text: 'sponsor',
-      links: [{link: 'sponsor', text: '', }]
-    }, {
-      text: 'contact us',
-      links: [{link: 'contact', text: '', }]
+      ],
     },
-  ]
+    {
+      text: 'blog',
+      links: [{ link: 'blog', text: '' }],
+    },
+    {
+      text: 'sponsor',
+      links: [{ link: 'sponsor', text: '' }],
+    },
+    {
+      text: 'contact us',
+      links: [{ link: 'contact', text: '' }],
+    },
+  ];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   goToLink(link: string) {
     this.router.navigate([`/${link}`]);
   }
 
-  // ngOnInit() {
-  // }
+  ngOnInit() {
+    this.checkWidth(window);
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWidth(event);
+  }
+
+  checkWidth(event: any) {
+    const width = event?.target?.innerWidth || event?.innerWidth;
+    this.isMobile = this.maxWidth > width;
+    if (!this.isMobile) {
+      this.sidebar?.close();
+    }
+  }
 }
--- wic-website-2022/src/app/modules/wic-toolbar/wic-toolbar.module.ts
+++ wic-website-2022/src/app/modules/wic-toolbar/wic-toolbar.module.ts
@@ -7,15 +7,19 @@ import {MatMenuModule} from '@angular/material/menu';
 import { MenuButtonModule } from '@app/shared/components/menu-button/menu-button.module';
 import { PipesModule } from '@app/shared/pipes/pipes.module';
 import { EventListItemModule } from '@app/shared/components/event-list-item/event-list-item/event-list-item.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
     MenuButtonModule,
     PipesModule,
+    MatIconModule,
     EventListItemModule,
   ],
   declarations: [WicToolbarComponent],
